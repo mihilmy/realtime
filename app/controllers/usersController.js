3980f2ce567f527fa8d9520374bcb012bf912ed2
Omar El-Mihilmy <mihilmy@yahoo.com>
2017-04-11 00:49:09 -0400

    fixed favorites

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -50,23 +50,18 @@ app.controller('usersController', ['$scope', '$rootScope', '$routeParams', '$fir
 	A mapper that finds the users favorites and then cross maps the keys to the actual post.
 	*/
 	$scope.favorites = function() {
-		var favsRef = db.child('favorites').child($routeParams.id);
-		var favs = $firebaseObject(favsRef);
 		var postsRef = db.child('posts');
-		var posts = [];
-		favs.$loaded().then(function() {
-			for(var key in favs) {
-				if (favs.hasOwnProperty(key) && !key.startsWith("$")) {
-					var p = $firebaseObject(postsRef.child(key));
-					p.$loaded().then(function() {
-						posts.push(p);
-						$scope.favPosts = posts;
-					});
-					
-				}
+		var postsArr = [];
+		
+		var favsRef = db.child('favorites').child($routeParams.id).once('value').then( function (favs) {
+			for(var key in favs.val()) {
+				postsRef.child(key).once('value').then( function(post) {
+					postsArr.push(post.val());
+				});
 			}
 		});
 		
+		$scope.favoritedPosts = postsArr;
 	}
 	
 }]);
